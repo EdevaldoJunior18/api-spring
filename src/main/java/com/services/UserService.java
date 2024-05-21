3a59d92480fa7242006872cd721fc251f2f0e851
Seu Nome <seu.email@example.com>
2024-05-21 09:13:24 -0300

    das

--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -1,14 +1,8 @@
 package com.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.entities.dto.UserDTO;
-import com.domain.repositories.IUserRepository;
-import com.domain.usecases.CreateUser;
-import com.domain.usecases.DeleteUser;
-import com.domain.usecases.FindUser;
-import com.domain.usecases.UpdateUser;
 
 /**
  *
@@ -17,32 +11,33 @@ import com.domain.usecases.UpdateUser;
 @Service
 public class UserService {
 
-    @Autowired
-    private IUserRepository userRepository;
-
     public UserDTO createUser(UserDTO dto) {
+        // var create = new CreateUser(userRepository);
+        // return create.execute(dto);
 
-        var create = new CreateUser(userRepository);
-        return create.execute(dto);
+        return null;
 
     }
 
     public UserDTO findUser(String document) {
-        var find = new FindUser(userRepository);
-        return find.execute(document);
+        // var find = new FindUser(userRepository);
+        // return find.execute(document);
 
+        return null;
     }
 
     public UserDTO updateUser(String document, String password) {
-        var update = new UpdateUser(userRepository);
-        return update.execute(document, password);
+        // var update = new UpdateUser(userRepository);
+        // return update.execute(document, password);
 
+        return null;
     }
 
     public UserDTO deleteUser(String document, String password) {
-        var delete = new DeleteUser(userRepository);
-        return delete.execute(document, password);
+        // var delete = new DeleteUser(userRepository);
+        // return delete.execute(document, password);
 
+        return null;
     }
 
 }
